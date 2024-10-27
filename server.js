d4d2532ec5f95027add74087a17dbc41aad7b953
Laura Tavares <144948480+LauraTavares2323@users.noreply.github.com>
2024-10-27 12:42:54 -0300

    server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: 'Mnelbt08@',
     database: 'nome_sobrenome_db'
 });
 
